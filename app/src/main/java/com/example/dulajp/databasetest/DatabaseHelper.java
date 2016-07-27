0deff4a9247e1c91e79c3fe4a7b5b193eb9e249c
Dulaj Perera <dulajp@IronOne.Local>
2016-07-27 14:31:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dulajp/databasetest/DatabaseHelper.java
@@ -0,0 +1,48 @@
+package com.example.dulajp.databasetest;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by DULAJP on 7/27/2016.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String  DATABASE_NAME = "student.db";
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL1 = "ID";
+    public static final String COL2 = "NAME";
+    public static final String COL3 = "SURNAME";
+    public static final String COL4 = "MARKS";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table "+TABLE_NAME+" (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,SURNAME TEXT,MARKS INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exist "+TABLE_NAME+"");
+        onCreate(db);
+    }
+
+    public boolean insertData(String name, String surname, String marks){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2,name);
+        contentValues.put(COL3,surname);
+        contentValues.put(COL4,marks);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1)
+            return  false;
+        else
+            return  true;
+    }
+}
---
+++ app/src/main/java/com/example/dulajp/databasetest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.dulajp.databasetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    EditText editName, editSurname, editMarks;
+    Button btnAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
+
+        editName = (EditText) findViewById(R.id.editText_name);
+        editSurname = (EditText) findViewById(R.id.editText_surname);
+        editMarks = (EditText) findViewById(R.id.editText_marks);
+        btnAdd = (Button) findViewById(R.id.button_add);
+    }
+
+    public void addData(){
+        btnAdd.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
+    }
+}
