98a6ddf70465710b942b24289f53a17c747cf32a
dulajperera <d.ayeshan@gmail.com>
2016-07-27 14:53:00 +0530

    completed Data insertion part

--- app/src/main/java/com/example/dulajp/databasetest/MainActivity.java
+++ app/src/main/java/com/example/dulajp/databasetest/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,7 +30,11 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                       boolean isInserted =  myDb.insertData(editName.getText().toString(),editSurname.getText().toString(),editMarks.getText().toString());
+                    if(isInserted){
+                        Toast.makeText(MainActivity.this,"Data inserted",Toast.LENGTH_LONG).show();
 
+                    }
                     }
                 }
         );
